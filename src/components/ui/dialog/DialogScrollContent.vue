ed4cf38dbad836b3a9f700f865974ecaeb706503
Satont <satontworldwide@gmail.com>
2024-03-10 17:59:36 +0300

    fix: correct build

--- src/components/ui/dialog/DialogScrollContent.vue
+++ src/components/ui/dialog/DialogScrollContent.vue
@@ -50,7 +50,7 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
         <DialogClose
           class="absolute top-4 right-4 p-0.5 transition-colors rounded-md hover:bg-secondary"
         >
-          <Cross2Icon class="w-4 h-4" />
+          <X class="w-4 h-4" />
           <span class="sr-only">Close</span>
         </DialogClose>
       </DialogContent>
